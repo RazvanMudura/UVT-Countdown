7b26121dd3f01cc732e69001d8d1bb159cc30ae0
Mudura Razvan <razvanmudura25@gmail.com>
2021-08-16 18:41:23 +0300

    changes

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 let countDownDate = new Date("Sep 20, 2021 09:00:00").getTime();
 let countdown = document.getElementById("countdown");
+let ended = false;
 
 const timer = () => {
   let today = new Date().getTime();
   let timeleft = countDownDate - today;
 
   if (timeleft < 0) {
-    countdown.remove();
-    let timeUp = document.createElement("div");
-    document.appendChild(timeUp);
-    timeUp.innerText = "Time is Up!";
+    if (!ended) {
+      countdown.remove();
+      let timeUp = document.createElement("div");
+      timeUp.innerText = "Gata Bro";
+      timeUp.style.fontSize = "5vh";
+      timeUp.id = "box";
+      timeUp.style.margin = "auto";
+      document.getElementById("container").appendChild(timeUp);
+      ended = true;
+    }
   } else {
     let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
     let hours = Math.floor(
