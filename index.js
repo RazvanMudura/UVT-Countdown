c87fb778385106548904499eaf12c532b85063ff
Mudura Razvan <razvanmudura25@gmail.com>
2021-11-26 01:30:37 +0200

    video

--- index.js
+++ index.js
@@ -10,18 +10,35 @@ const timer = () => {
     if (!ended) {
       countdown.remove();
       let timeUp = document.createElement("div");
-      timeUp.innerText = "Gata Bro";
-      timeUp.style.fontSize = "5vh";
+      let video = document.createElement("video");
+      let audio = document.createElement("audio");
+
+      audio.src = "audio.mp3";
+      audio.loop = true;
+      audio.autoplay = true;
+
+      video.src = "video.mp4";
+      video.loop = true;
+      video.muted = true;
+      video.autoplay = true;
+
+      timeUp.innerText = "Gata cu joaca bro \n Du-te si salveza-ti patria";
       timeUp.id = "box";
       timeUp.style.margin = "auto";
+      timeUp.style.width = "50%";
+      timeUp.style.fontSize = "40px";
+
       document.getElementById("container").appendChild(timeUp);
+      document.getElementById("container").appendChild(video);
+      document.getElementById("container").addEventListener("mouseover", () => {
+        audio.play();
+      });
+
       ended = true;
     }
   } else {
     let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    let hours = Math.floor(
-      (timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
+    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
